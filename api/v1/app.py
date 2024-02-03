9dfe8706a840fd449358d441bdd5c7fa1d5b6af0
AnsaYong <ansahmbomyong@gmail.com>
2024-02-03 15:40:28 +0200

    added a method to app.py to handle error messages

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ and runs app.
 from models import storage
 import os
 from api.v1.views import app_views
-from flask import Flask, Blueprint
+from flask import Flask, make_response, jsonify
 
 
 app = Flask(__name__)
@@ -17,6 +17,13 @@ app.url_map.strict_slashes = False
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    """Handles 404 error and returns a JSON-formatted 404 status code
+    response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
