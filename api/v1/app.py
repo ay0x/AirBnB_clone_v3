4201f76c2951ffe495eac5a5801c2ecbaa6dfdc7
AnsaYong <ansahmbomyong@gmail.com>
2024-02-03 14:42:49 +0200

    updated files to meet pycodestyle standards

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-
+"""This module provides a method tht handles teardown
+and runs app.
+"""
 from models import storage
 import os
 from api.v1.views import app_views
 from flask import Flask, Blueprint
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
