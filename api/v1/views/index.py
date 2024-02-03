d4e4dd838d34774490a8c6b049ea3042ce37f1d8
AnsaYong <ansahmbomyong@gmail.com>
2024-02-03 14:09:37 +0200

    updated vews.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,11 @@
 """This module creates a route that returns the
 status of our API.
 """
-from api.v1.views import app_views
+from app.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status", method=["GET"])
-def status():
+@app_views.route("/status", methods=["GET"])
+def get_status():
     """Returns the status of our API"""
-    return (jsonify({"status": "OK"}))
+    return jsonify({"status": "OK"})
