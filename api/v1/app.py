4abc1d5b9c0c859322d0d5fb28b3da89800f0151
AnsaYong <ansahmbomyong@gmail.com>
2024-02-03 13:24:24 +0200

    updated files

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-
+"""Provides a method that handles teardown by calling
+storage.close().
+"""
 from models import storage
 import os
 from api.v1.views import app_views
 from flask import Flask, Blueprint
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
+"""This module creates an instance of a
+Blueprint.
+"""
 from flask import Blueprint
 from api.v1.views.index import *
+
 
 app_views = Blueprint(url_prefix="/api/v1")
