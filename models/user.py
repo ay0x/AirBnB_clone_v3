45431f39f17e1836cee30774ce60b85fe3ce2293
ay0x <85720179+ay0x@users.noreply.github.com>
2024-02-06 04:32:00 +0200

    Updated password security user.py and base_model.py

--- models/user.py
+++ models/user.py
@@ -3,9 +3,9 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +27,12 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = hashlib.md5(pwd.encode()).hexdigest()
